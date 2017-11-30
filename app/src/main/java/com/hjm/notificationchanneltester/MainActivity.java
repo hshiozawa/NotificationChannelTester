052cc7ab63354cc2f7621e2d98a271d1bd68f833
Hajime Shiozawa <shiozawa@ripplex.com>
2017-11-30 11:54:25 +0900

    implement buttons

--- app/src/main/java/com/hjm/notificationchanneltester/MainActivity.java
+++ app/src/main/java/com/hjm/notificationchanneltester/MainActivity.java
@@ -1,7 +1,9 @@
 package com.hjm.notificationchanneltester;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button1 = (Button) findViewById(R.id.channel_1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO show notification
+            }
+        });
+
+        Button button2 = (Button) findViewById(R.id.channel_2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO show notification
+            }
+        });
+
+        Button button3 = (Button) findViewById(R.id.channel_3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO show notification
+            }
+        });
     }
+
 }
+
