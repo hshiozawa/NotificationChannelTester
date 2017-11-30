7f969fc2d6d3f9300293d89e5ccc5d7a2760c961
Hajime Shiozawa <shiozawa@ripplex.com>
2017-11-30 14:47:30 +0900

    targetSdkVersion 25 以下の場合で動作する通知を実装

--- app/src/main/java/com/hjm/notificationchanneltester/MainActivity.java
+++ app/src/main/java/com/hjm/notificationchanneltester/MainActivity.java
@@ -1,6 +1,10 @@
 package com.hjm.notificationchanneltester;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -12,11 +16,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final String notificationTitle = getResources().getText(R.string.app_name).toString();
+
         Button button1 = (Button) findViewById(R.id.channel_1);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO show notification
+                showNotification(notificationTitle, "Channel 1");
             }
         });
 
@@ -24,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO show notification
+                showNotification(notificationTitle, "Channel 2");
             }
         });
 
@@ -32,9 +38,22 @@ public class MainActivity extends AppCompatActivity {
         button3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO show notification
+                showNotification(notificationTitle, "Channel 3");
             }
         });
+    }
+
+    private void showNotification(String title, String message) {
+        Context context = getApplicationContext();
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+        builder.setSmallIcon(R.drawable.icon)
+                .setContentTitle(title)
+                .setContentText(message);
+
+        NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        Notification notification = builder.build();
+        manager.notify(1, notification);
     }
 
 }
